9aac31325f005dda1c905f74e2c8506f4a983759
Sahar Adem <sahar.adem95@gmail.com>
2020-11-03 10:41:52 +0300

    add edit button and sort the list of notes

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let storedNotes = JSON.parse(localStorage.getItem("notes"));
-
 let notes = storedNotes ? storedNotes : [];
 let list = document.getElementById("list");
+let divEdit = document.getElementById("edit-note");
 
 showNotes();
 document.getElementById("add-btn").addEventListener("click", function () {
   let title = document.getElementById("title").value;
   let description = document.getElementById("description").value;
-  console.log(description, "des");
 
   if (title === "") {
     alert("pleas enter the title of the note");
@@ -15,17 +14,17 @@ document.getElementById("add-btn").addEventListener("click", function () {
     notes.push({ title: title, description: description });
     document.getElementById("title").value = "";
     document.getElementById("description").value = "";
-
-    console.log(notes, "notes");
     showNotes();
   }
 });
 
 function showNotes() {
+  sortNotes();
   list.innerHTML = "";
 
   notes.map(function (note, i) {
     let listItem = document.createElement("LI");
+    listItem.setAttribute("id", i);
 
     let divTitle = document.createElement("DIV");
     divTitle.textContent = note.title;
@@ -48,11 +47,65 @@ function showNotes() {
       }
     });
 
+    let EditnoteBtn = document.createElement("BUTTON");
+    let EditBtnText = document.createTextNode("Edit");
+
+    EditnoteBtn.appendChild(EditBtnText);
+
+    EditnoteBtn.addEventListener("click", function () {
+      if (document.querySelector("#list li.selected") != null) {
+        document
+          .querySelector("#list li.selected")
+          .classList.remove("selected");
+      }
+
+      divEdit.classList.remove("hide");
+      listItem.classList.add("selected");
+
+      document.getElementById("save").addEventListener("click", function () {
+        let title = document.getElementById("edit-title").value;
+        let description = document.getElementById("edit-description").value;
+
+        if (document.querySelector("#list li.selected") != null) {
+          selectedId = document
+            .querySelector("#list li.selected")
+            .getAttribute("id");
+
+          notes.map((note, i) => {
+            if (selectedId == i) {
+              note.title = title;
+              note.description = description;
+              divEdit.classList.add("hide");
+              document.getElementById("edit-title").value = "";
+              document.getElementById("edit-description").value = "";
+              listItem.classList.remove("selected");
+              showNotes();
+            }
+          });
+        }
+      });
+    });
+
     listItem.appendChild(divTitle);
     listItem.appendChild(divDescription);
     listItem.appendChild(deleteNoteBtn);
+    listItem.appendChild(EditnoteBtn);
 
     list.appendChild(listItem);
   });
   localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+function sortNotes() {
+  notes.sort(function (a, b) {
+    var nameA = a.title.toUpperCase();
+    var nameB = b.title.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
 }
